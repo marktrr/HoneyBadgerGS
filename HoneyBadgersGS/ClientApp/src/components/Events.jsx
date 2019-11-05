624cb8563ce9c49b844605c85c30ce54250a259a
Nathan Hannon <nathan@hannon.me>
2019-11-05 11:30:36 -0500

    Changed extensions

--- HoneyBadgersGS/ClientApp/src/components/Events.jsx
+++ HoneyBadgersGS/ClientApp/src/components/Events.jsx
@@ -6,5 +6,23 @@ export class Event extends Component
     constructor()
     {
         super();
+        this.state={
+            events:[]
+        };
+    }
+    componentDidMount()
+    {
+        fetch("https://localhost:5001/api/events/getEvents")
+        .then(response => response.json())
+        .then(data => this.setState({ events: data }))
+            console.log(this.state.events);
+    }
+    render()
+    {
+        return(
+            <div class = "event-method">
+                <EventList>events={this.state.events}</EventList>
+            </div>
+        )
     }
 }
