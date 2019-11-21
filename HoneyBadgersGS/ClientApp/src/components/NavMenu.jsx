3b2df941d7ee4d99bcd501664b1b058cd638311c
Nathan Hannon <nathan@hannon.me>
2019-11-21 14:03:40 -0500

    removed search bar

--- HoneyBadgersGS/ClientApp/src/components/NavMenu.jsx
+++ HoneyBadgersGS/ClientApp/src/components/NavMenu.jsx
@@ -29,9 +29,6 @@ export class NavMenu extends Component {
         <Navbar className="navbar-expand-sm navbar-toggleable-sm box-shadow mb-3" light>
           <Container>
                     <NavbarBrand tag={Link} to="/"><img src={Logo} className="honeyBadgerLogo" alt="Honey Badger Logo"></img></NavbarBrand>
-                    <form>
-                        <input className="storeSearch mr-sm-2" type="text" placeholder="Search"></input>
-                    </form>
             <NavbarToggler id="hamburger" onClick={this.toggleNavbar} className="mr-2"  />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar >
               <ul className="navbar-nav flex-grow">
