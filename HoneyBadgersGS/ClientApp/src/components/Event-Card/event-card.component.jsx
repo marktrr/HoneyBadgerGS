f556052b8682a5e41fe93ba29fbe82683774cfb7
Nathan Hannon <nathan@hannon.me>
2019-11-22 17:42:17 -0500

    removed display props

--- HoneyBadgersGS/ClientApp/src/components/Event-Card/event-card.component.jsx
+++ HoneyBadgersGS/ClientApp/src/components/Event-Card/event-card.component.jsx
@@ -5,7 +5,6 @@ export const EventCard = props =>{
     return (
         <div className="event-card">
             <p>{props.events.eventId}</p>
-            <p>{props.events.eventName}</p>
             <p>{props.events.eventDate}</p>
             <p>{props.events.eventDescription}</p>
             <p>{props.events.eventLocation}</p>
