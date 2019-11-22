38b6f349a0d1f3a456d06bbe92327c5a09af5d2f
Kevin Lucas <kevinleo.lucas@gmail.com>
2019-11-22 01:40:26 -0500

    reload window when the profile is update/ added. Add testing for the profile, game, and review.

---
+++ HoneyBadgerTest/GameControllerTests/MockGameController.cs
@@ -0,0 +1,121 @@
+﻿using HoneyBadgers._0.BusinessLogic;
+using HoneyBadgers._0.Controllers;
+using HoneyBadgers._0.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace HoneyBadgerTest
+{
+	
+	public class MockGameController 
+	{
+		GamesController _controller;
+		IGameLogic _gameLogic;
+
+		public MockGameController()
+		{
+			_gameLogic = new MockGameLogic();
+			_controller = new GamesController(_gameLogic);
+		}
+
+
+		//get all the product , expect the list return to match.
+		[Fact]
+		public void GetGames_ReturnsOkResult()
+		{
+			//Act
+			var okResult = _controller.GetAllGames();
+			//Assert
+			Assert.Equal(3, okResult.Count());
+		}
+
+		// sends a game id which does exist, expects both game ids to match.
+		[Fact]
+		public void GetAGameWithExistingId_ReturnGameWithCorrectId()
+		{
+			//Arrange
+			var okResult = _controller.Details(1);
+			int expected_Id = 1;
+			//Act
+			Assert.Equal( expected_Id, okResult.GameId);		
+		}
+
+		//Sends a game id which doesn't exist, expects a null instead of the  game object.
+		[Fact]
+		public void GetAGameWithNonExistingId_()
+		{
+			//Arrange
+			var okResult = _controller.Details(5);
+			Game ? expected = null;
+			//Act
+			Assert.Equal(expected, okResult);
+		}
+
+		//add
+		[Fact]
+		public void AddValidGame_ExpectOne()
+		{
+
+			Game newGame = new Game
+			{
+
+				GameId = 4,
+				GameName = " randomGame",
+				Publisher = "Activision",
+				Developer = "Etranges Libellules",
+				Genre = "Platformer",
+				Platform = "All",
+				GameDescription = "SOme new game",
+				GameArtUrl = "https://www.giantbomb.com/images/1300-2136555",
+				ReleaseDate = new DateTime(2008, 10, 21),
+				Price = 50.54
+
+			};
+
+			int result = _controller.Add(newGame);
+			Assert.Equal(1, result);
+
+		}
+
+		[Fact]
+		public void UpdateValidGame_ExpectOne()
+		{
+
+			Game newGame = new Game
+			{
+
+				GameId = 1,
+				GameName = " updatedGame",
+				Publisher = "Activision",
+				Developer = "Etranges Libellules",
+				Genre = "Platformer",
+				Platform = "All",
+				GameDescription = "SOme new game",
+				GameArtUrl = "https://www.giantbomb.com/images/1300-2136555",
+				ReleaseDate = new DateTime(2008, 10, 21),
+				Price = 50.54
+
+			};
+			int result = _controller.Update(newGame);
+			Assert.Equal(0, result);
+		}
+		[Fact]
+		public void DeleteGame_ExpectOne()
+		{
+			int id = 1;
+			int result = _controller.Delete(id);
+			Assert.Equal(1, result);
+		}
+
+
+
+
+	}
+}
---
+++ HoneyBadgerTest/ProfileControllerTests/MockProfileLogic.cs
@@ -0,0 +1,114 @@
+﻿using HoneyBadgers._0.BusinessLogic;
+using HoneyBadgers._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoneyBadgerTest.ProfileControllerTests
+{
+	public class MockProfileLogic : IProfileLogic
+	{
+
+		//create example data which will be used to test the controller.
+		private readonly List<Profile> _ProfileList;
+
+
+		//public  method that returns a list of profile objects.
+		public List<Profile> TestProfiles()
+		{
+			List<Profile> TestProfiles_list = new List<Profile>();
+			TestProfiles_list.Add(new Profile
+			{
+				ActualName = "Jimmy",
+				DisplayName = "Dragon",
+				Dob = new DateTime(1987,09,20),
+				Email ="JimmyTheDragon@gmail.com",
+				Gender = "Male",
+				ProfileId = "763adcfc-2abc-493a-b35d-813d95905906",
+				ProfileImage = null,
+				Promotion = true,
+				UserAddress = "600 Golden Acre Valley"
+
+
+			});
+			TestProfiles_list.Add(new Profile
+			{
+				ActualName = "Jenny",
+				DisplayName = "JFenny",
+				Dob = new DateTime(2000, 11, 10),
+				Email = "JJEnders@hotmail.ca",
+				Gender = "Female",
+				ProfileId = "f9d2aad6-2b4a-4740-84f2-737fc3d06a76",
+				ProfileImage = null,
+				Promotion = false,
+				UserAddress = "19 Oak Drive"
+			});
+			TestProfiles_list.Add(new Profile
+			{
+				ActualName = "Danny",
+				DisplayName = "DanTheMan",
+				Dob = new DateTime(1975, 01, 15),
+				Email = "DMan@hotmai.com",
+				Gender = "Male",
+				ProfileId = "8ed22a94-fbdd-4014-8824-3bcb5e80792a",
+				ProfileImage = null,
+				Promotion = false,
+				UserAddress = "100 Oscar Ave"
+			});
+			return TestProfiles_list;
+		}
+
+		//get the list of test profiles
+		public MockProfileLogic()
+		{
+			_ProfileList = TestProfiles();
+		}
+
+		public bool Add(Profile profile)
+		{
+			profile.ProfileId = Guid.NewGuid().ToString();
+			_ProfileList.Add(profile);
+			return true;
+		}
+
+		public int Delete(string id)
+		{
+			try
+			{
+				Profile profileToBeDeleted = _ProfileList.Find(a => a.ProfileId == id);
+				_ProfileList.Remove(profileToBeDeleted);
+				return 1;
+			}
+			catch (Exception e)
+			{
+				return 0;
+			}
+			
+		}
+
+		public Profile Details(string id)
+		{
+			return _ProfileList.Find(x => x.ProfileId == id);
+		}
+
+		public IEnumerable<Profile> GetAll()
+		{
+			return _ProfileList;
+		}
+
+		public bool Update(Profile profile)
+		{
+			try
+			{
+				int found_profile_index = _ProfileList.FindIndex(x => x.ProfileId == profile.ProfileId);
+				_ProfileList[found_profile_index] = profile;
+				return true;
+			}
+			catch(Exception e)
+			{
+				return false;
+			}
+			
+		}
+	}
+}
